9b176e5a0424e596abc088b76212dc8dcf6f156c
nmartinson <nick-martinson@uiowa.edu>
2014-10-05 23:52:10 -0300

    Part 2 is finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131107031651) do
+ActiveRecord::Schema.define(:version => 20141006003532) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(:version => 20131107031651) do
     t.datetime "release_date"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "user_id"
+    t.string   "session_token"
+    t.datetime "timestamps"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
